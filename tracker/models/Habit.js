f4527a31d7266d2c40826b2099c131cee5e624e2
Nutod <alukodotun@gmail.com>
2020-09-14 21:57:04 +0700

    Added base schema definition to Habit

--- tracker/models/Habit.js
+++ tracker/models/Habit.js
@@ -1,1 +1,14 @@
 const mongoose = require("mongoose")
+
+const HabitSchema = new mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  completed: {
+    required: true,
+    type: Boolean,
+  },
+})
+
+module.exports = mongoose.model("Habit", HabitSchema)
