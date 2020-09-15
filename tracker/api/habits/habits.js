cbf70ae9ebed2255ea527e7571d65e6737ae6703
Nutod <alukodotun@gmail.com>
2020-09-15 14:48:37 +0700

    Added new collection definition for habits

--- tracker/api/habits/habits.js
+++ tracker/api/habits/habits.js
@@ -1,1 +1,11 @@
 // DB Collection
+import mongoose from "mongoose"
+
+export const HabitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+})
+
+export default mongoose.model.Habit || mongoose.model("Habit", HabitSchema)
